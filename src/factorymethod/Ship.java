f8f102ed0970f130a3dd279e77a060bf10c37e89
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-19 13:54:22 +0500

    updated

--- src/factorymethod/Ship.java
+++ src/factorymethod/Ship.java
@@ -2,6 +2,6 @@ package factorymethod;
 
 public class Ship implements Transport{
     public void deliver(String cargo , String destination){
-        System.out.println("Ship delivers" + cargo + "to" + destination);
+        System.out.println("Ship delivers " + cargo + " to " + destination);
     }
 }
