b324b665bcd34f96cc83271cf665554978b87ec6
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-19 13:20:57 +0500

    add ship class

--- src/factorymethod/Ship.java
+++ src/factorymethod/Ship.java
@@ -1,4 +1,7 @@
 package factorymethod;
 
-public class Ship {
+public class Ship implements Transport{
+    public void deliver(String cargo , String destination){
+        System.out.println("Ship delivers" + cargo + "to" + destination);
+    }
 }
