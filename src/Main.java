3eb3b89a7484df745bc8ce8c60520941bf6d289b
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-19 13:52:11 +0500

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import factorymethod.Logistics;
+import factorymethod.RoadLogistics;
+import factorymethod.SeaLogistics;
 public class Main {
+    public static void main(String[] args){
+        Logistics road = new RoadLogistics();
+        road.planDelivery("Equipment" , "Astana");
+
+        Logistics sea = new SeaLogistics();
+        sea.planDelivery("Cars" , "Almaty");
+    }
 }
