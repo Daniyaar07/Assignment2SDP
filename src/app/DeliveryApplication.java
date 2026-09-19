9d67ba8ff4c0a60cbb3ca6e401f0e2f7df6a6447
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-19 14:12:50 +0500

    add app

--- src/app/DeliveryApplication.java
+++ src/app/DeliveryApplication.java
@@ -1,4 +1,21 @@
 package app;
-
+import abstractfactory.Button;
+import abstractfactory.Checkbox;
+import abstractfactory.GUIFactory;
+import factorymethod.Logistics;
 public class DeliveryApplication {
+    private Button button;
+    private Checkbox checkbox;
+    private Logistics logistics;
+
+    public DeliveryApplication(GUIFactory factory , Logistics logistics){
+        button = factory.createButton();
+        checkbox = factory.createCheckbox();
+        this.logistics = logistics;
+    }
+    public void run(String cargo , String destination){
+        button.paint();
+        checkbox.paint();
+        logistics.planDelivery(cargo, destination);
+    }
 }
