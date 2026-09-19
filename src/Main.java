d00146d5dae6c6fa882acdb352623bbec9dddf9b
Daniyar <daniyarbaikanov07@gmail.com>
2026-09-19 14:13:11 +0500

    add user input

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,44 @@
 import factorymethod.Logistics;
 import factorymethod.RoadLogistics;
 import factorymethod.SeaLogistics;
+import java.util.Scanner;
+import abstractfactory.GUIFactory;
+import abstractfactory.WindowsFactory;
+import abstractfactory.MacOSFactory;
+import app.DeliveryApplication;
 public class Main {
     public static void main(String[] args){
-        Logistics road = new RoadLogistics();
-        road.planDelivery("Equipment" , "Astana");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input delivery mode: ");
+        String deliveryMode = scanner.nextLine();
 
-        Logistics sea = new SeaLogistics();
-        sea.planDelivery("Cars" , "Almaty");
+        System.out.println("Input UI platform: ");
+        String platform = scanner.nextLine();
+
+        Logistics logistics;
+        if (deliveryMode.equalsIgnoreCase("ROAD")){
+            logistics = new RoadLogistics();
+        }
+        else if (deliveryMode.equalsIgnoreCase("SEA")){
+            logistics = new SeaLogistics();
+        }
+        else{
+            System.out.println("Unsupported delivery mode");
+            return;
+        }
+
+        GUIFactory factory;
+        if (platform.equalsIgnoreCase("WINDOWS")){
+            factory = new WindowsFactory();
+        }
+        else if(platform.equalsIgnoreCase("MACOS")){
+            factory = new MacOSFactory();
+        }
+        else{
+            System.out.println("Unsupported UI platform");
+            return;
+        }
+        DeliveryApplication app = new DeliveryApplication(factory , logistics);
+        app.run("Equipment" , "Astana");
     }
 }
